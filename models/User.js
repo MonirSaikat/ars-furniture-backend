8a6346a5fc81e88c8dfd2e4614bc291ea6970b33
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-02 17:35:16 +0600

    added address field

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  address: { type: String, default: '' }
 });
 
 userSchema.methods.validatePassword = async function(password){
